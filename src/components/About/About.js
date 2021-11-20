f97eeaa4c2892e2ae0a17fa265c94abb0d3a70f2
tahermamun <tahermamun5@gmail.com>
2021-11-20 13:57:07 +0600

    fix container

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ export const About = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-2 md:p-0">
           <div>
             <h1 className="text-3xl font-bold letter-space-2 py-2 mb-2 md:mb-8">
-              Why Netron<span className="text-pink-600">Soft</span>
+              Why Netron<span className="text-red-500">Soft</span>
             </h1>
             <p className="text-base font-thin pr-4 md:pr-10 letter-space-1">
             Our Business model is very flexible and we don't want to force you into anything that will not work with your business. We have worked with almost 100+ companies. We have achieved a lot since the beginning. We are provided with the best support to our customers. Our staff are available 24 hours and they can assist with anything your need, so you never have to worry about being left out on something important.
