eadc9635743ba866edcfe6f530cdc145d677d6b9
anondobormon <anondokpi@gmail.com>
2021-11-20 12:57:16 +0600

    added some features

--- src/components/AllServices/ServicesContent.js
+++ src/components/AllServices/ServicesContent.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-export const ServicesContent = ({item}) => {
+export const ServicesContent = ({ item }) => {
   return (
     <div className="bg-white px-2 md:px-6 py-2 md:py-8">
       <h1 className="text-2xl letter-space-2 font-bold py-2">
-       {item.serviceTitle}
+        {item.serviceTitle}
       </h1>
-      <div className="h-1 w-52 bg-pink-600"></div>
-      <p className="text-xl py-4 letter-space-1">
-      {item.serviceDescription}
-      </p>
+      <div className="h-1 w-52 bg-red-500"></div>
+      <p className="text-xl py-4 letter-space-1">{item.serviceDescription}</p>
     </div>
   );
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,7 @@
 import { BeakerIcon } from "@heroicons/react/solid";
 import React, { useState } from "react";
 import { Title } from "../SectionTitle/Title";
-import './Contact.css'
-
+import "./Contact.css";
 
 export const Contact = () => {
   const [formData, setFormData] = useState({});
@@ -14,7 +13,7 @@ export const Contact = () => {
   };
   console.log(formData);
 
-  const handleSubmit = () => { };
+  const handleSubmit = () => {};
   return (
     <div className=" py-8">
       <Title title1={"Contact Us"} title2={"Connect Our Team"} />
@@ -23,7 +22,7 @@ export const Contact = () => {
           <div className="pt-6 pb-6 md:pl-6 md:col-span-2 pl-0 ">
             <div className="bg-white rounded p-0 md:pl-16 md:pt-16 md:pb-16 md:pr-0 md:pr-16 ">
               <div className="p-4 ">
-                <h1 className="text-4xl font-semibold letter-space-2">
+                <h1 className="text-4xl mb-5 md:mb-10 font-semibold letter-space-2">
                   Get In Touch
                 </h1>
                 <div className="grid grid-cols-1 my-2 md:grid-cols-2 gap-4">
@@ -31,7 +30,7 @@ export const Contact = () => {
                     type="text"
                     name="name"
                     onBlur={handleBlur}
-                    className="p-2 rounded border border_custom_shadow"
+                    className="p-4 focus:outline-none rounded border border_custom_shadow"
                     placeholder="Name"
                     required
                   />
@@ -39,7 +38,7 @@ export const Contact = () => {
                     type="email"
                     name="email"
                     onBlur={handleBlur}
-                    className="p-2 rounded border border_custom_shadow"
+                    className="p-4 focus:outline-none rounded border border_custom_shadow"
                     placeholder="Email"
                     required
                   />
@@ -48,26 +47,26 @@ export const Contact = () => {
                   type="text"
                   onBlur={handleBlur}
                   name="subject"
-                  className="p-2 rounded w-full my-2 border border_custom_shadow"
+                  className="p-4 focus:outline-none rounded w-full my-2 border border_custom_shadow"
                   placeholder="Subject"
                   required
                 />
                 <textarea
                   name="message"
                   onBlur={handleBlur}
-                  className="p-4 h-42 md:h-52 lg:h-42 my-2 rounded w-full border border_custom_shadow"
+                  className="p-4 h-42 focus:outline-none md:h-52 lg:h-42 my-2 rounded w-full border border_custom_shadow"
                   placeholder="Message"
                 ></textarea>
                 <button
                   onClick={handleSubmit}
-                  className="p-2 rounded flex items-center justify-center bg-pink-500 my-2 text-white border_custom_shadow"
+                  className="p-4 rounded flex items-center justify-center bg-red-500 my-2 text-white border_custom_shadow"
                 >
                   Send Message
                 </button>
               </div>
             </div>
           </div>
-          <div className="bg-pink-500 p-4 md:p-10 rounded text-white">
+          <div className="bg-red-500 p-4 md:p-10 rounded text-white">
             <h1 className="text-3xl font-semibold my-4 text-center letter-space-2">
               Contact Us
             </h1>
@@ -90,7 +89,6 @@ export const Contact = () => {
               </div>
               <div className="">
                 <p className="text-base font-bold">
-
                   <span className="ml-2 font-thin">+881234567897412</span>
                 </p>
               </div>
@@ -102,7 +100,6 @@ export const Contact = () => {
               </div>
               <div className="">
                 <p className="text-base font-bold">
-
                   <span className="ml-2 font-thin">abgced@mail.com</span>
                 </p>
               </div>
@@ -114,7 +111,6 @@ export const Contact = () => {
               </div>
               <div className="">
                 <p className="text-base font-bold">
-
                   <span className="ml-2 font-thin">netronsoft.netlify/app</span>
                 </p>
               </div>
