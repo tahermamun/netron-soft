027c8112bea1163c60cb7a779cb1bcde549f5ce7
tahermamun <tahermamun5@gmail.com>
2021-11-20 13:13:55 +0600

    service page error fix

--- src/components/AllServices/ServicesAllCard.js
+++ src/components/AllServices/ServicesAllCard.js
@@ -1,11 +1,30 @@
 import React from "react";
-
+import './AllServices.css'
 export const ServicesAllCard = ({ image, content, index }) => {
   console.log(index % 2 == 0);
   return (
-    <div className="main_container m-auto grid grid-cols-1 md:grid-cols-2">
-      {index % 2 == 0 ? content : image}
-      {index % 2 == 0 ? image : content}
+
+    <div className="main_container m-auto ">
+    <div className="computer_view">
+      <div className="grid m-auto md:grid-cols-2 justify-center items-center">
+        {index % 2 == 0 ? content : image}
+        {index % 2 == 0 ? image : content}
+      </div>
     </div>
+    <div className="mobile_view ">
+     <div className="mobile_view_grid">
+     {index === index == 0 ? content : image}
+      {/* {index % 2 == 0 ? image : content} */}
+      {index === index == 0 ? image : content}
+     </div>
+
+    </div>
+
+
+  </div>
+
+
   );
 };
+
+
